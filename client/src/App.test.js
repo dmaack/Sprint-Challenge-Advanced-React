d6a16c14e638df346901b4875e3537760a36deec
dmaack <dominiquemaack@gmail.com>
2019-10-04 12:46:50 -0400

    two testing running and working

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as rtl from '@testing-library/react'
+import { render } from '@testing-library/react'
 import App from './App';
+import Header from './components/Header';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+ it('App renders without crashing', () => {
+  render( <App /> )
+ });
+
+it('renders an h1 with the text Womens World Cup Player Roster', () => {
+  const wrapper = rtl.render(
+    <Header />
+  )
+  const element = wrapper.queryByText(/womens world cup player roster/i)
+  // expect(element).toBeInTheDocument();
+  expect(element).toBeTruthy();
+})
