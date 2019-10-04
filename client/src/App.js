a1175ef2857c1277d009c5f5de23879423ed5a97
dmaack <dominiquemaack@gmail.com>
2019-10-04 11:40:57 -0400

    receiving data from API

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
-
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      players: []
+    }
+  }
+
+  componentDidMount() {
+    console.log('cDM invoked in app.js.... calling setState')
+    console.log(this)
+
+    axios
+    .get('http://localhost:5000/api/players')
+    .then(res => {
+      this.setState({
+        players: res.data
+      })
+      console.log(res)
+    })
+  .catch(err => {
+    console.log('data did not render', err)
+  })
   }
 
   render() {
